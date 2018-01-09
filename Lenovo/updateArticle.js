02618154c747c04625c3973a2646c614953a4bab
fansaly <fansaly@gmail.com>
2018-01-09 08:00:06 +0800

    rename

--- Lenovo/updateArticle.js
+++ Lenovo/updateArticle.js
@@ -7,5 +7,5 @@
  */
 require('../updateArticle')({
   url: 'https://www.tonymacx86.com/threads/guide-lenovo-z50-70-z40-70-using-clover-uefi.232823/',
-  file: `${__dirname}/Lenovo.html`
+  file: `${__dirname}/Lenovo.txt`
 })
--- Strix/updateArticle.js
+++ Strix/updateArticle.js
@@ -7,5 +7,5 @@
  */
 require('../updateArticle')({
   url: 'https://www.tonymacx86.com/threads/skylake-x-x299-live-the-future-now-on-macos-high-sierra-10-13-successful-build-extended-guide.229353/',
-  file: `${__dirname}/Strix.html`
+  file: `${__dirname}/Strix.txt`
 })
